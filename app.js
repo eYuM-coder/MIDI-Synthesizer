d490b4fdc3e1c31f4335f447b72bd4a45e95972e
Boeing-737-800 <nathaniel2007w@gmail.com>
2023-07-05 13:22:16 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,96 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+let ctx;
+const startButton = document.querySelector('button');
+const oscillators = {};
+
+startButton.addEventListener('click', () => {
+    ctx = new AudioContext();
+    console.log(ctx);
+});
+
+
+function midiToFreq(number) {
+    const a = 440;
+    return (a / 32) * (2 ** ((number - 9) / 12));
+}
+
+if (navigator.requestMIDIAccess) {
+    navigator.requestMIDIAccess().then(success, fail)
+}
+
+function success(midiAccess) {
+    midiAccess.addEventListener('statechange', updateDevices);
+
+    const inputs = midiAccess.inputs;
+
+    inputs.forEach((input) => {
+        input.addEventListener('midimessage', handleInput);
+    });
+}
+
+function handleInput(input) {
+    const command = input.data[0];
+    const note = input.data[1];
+    const velocity = input.data[2];
+    
+    switch (command) {
+        case 144:
+        if (velocity > 0) {
+            noteOn(note, velocity);
+        } else {
+            noteOff(note);
+        }
+        break;
+        case 128:
+            noteOff(note);
+            break;
+    }
+}
+
+function noteOn(note, velocity) {
+    const osc = ctx.createOscillator();
+
+    const oscGain = ctx.createGain();
+    oscGain.gain.value = 0.33;
+
+    const velocityGainAmount = (1 / 127) * velocity;
+    const velocityGain = ctx.createGain();
+    velocityGain.gain.value = velocityGainAmount;
+
+    osc.type = "sawtooth";
+    osc.frequency.value = midiToFreq(note);
+
+    osc.connect(oscGain);
+    oscGain.connect(velocityGain);
+    velocityGain.connect(ctx.destination);
+
+    osc.gain = oscGain;
+
+    oscillators[note.toString()] = osc;
+    console.log(oscillators);
+    osc.start();
+}
+
+function noteOff(note) {
+    const osc = oscillators[note.toString()];
+    const oscGain = osc.gain;
+
+    oscGain.gain.setValueAtTime(oscGain.gain.value, ctx.currentTime);
+    oscGain.gain.exponentialRampToValueAtTime(0.0001, ctx.currentTime + 0.0001);
+
+    setTimeout(() => {
+        osc.stop();
+        osc.disconnect();
+    }, 2000);
+    delete oscillators[note.toString()];
+    console.log(oscillators);
+}
+
+function fail() {
+    console.log("Could not connect MIDI");
+}
+
+function updateDevices(event) {
+    console.log(event);
+    console.log(`Name: ${event.port.name}, Brand: ${event.port.manufacturer}, State: ${event.port.state}, Type: ${event.port.type}`);
+}
