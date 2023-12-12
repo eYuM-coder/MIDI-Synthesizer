2d006d8cd7f817ebd67ce7ff5da358dc4c3e46e5
Replit user <>
2023-12-12 06:03:47 +0000

    a

--- app.js
+++ app.js
@@ -1,100 +1,101 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 let ctx;
-const startButton = document.querySelector('button');
+const startButton = document.querySelector("button");
 const oscillators = {};
 
-startButton.addEventListener('click', () => {
-    ctx = new AudioContext();
-    console.log(ctx);
+startButton.addEventListener("click", () => {
+  ctx = new AudioContext();
+  console.log(ctx);
 });
 
-
 function midiToFreq(number) {
-    const a = 440;
-    return (a / 32) * (2 ** ((number - 9) / 12));
+  const a = 440;
+  return (a / 32) * 2 ** ((number - 9) / 12);
 }
 
 if (navigator.requestMIDIAccess) {
-    navigator.requestMIDIAccess().then(success, fail)
+  navigator.requestMIDIAccess().then(success, fail);
 }
 
 function success(midiAccess) {
-    midiAccess.addEventListener('statechange', updateDevices);
+  midiAccess.addEventListener("statechange", updateDevices);
 
-    const inputs = midiAccess.inputs;
+  const inputs = midiAccess.inputs;
 
-    inputs.forEach((input) => {
-        input.addEventListener('midimessage', handleInput);
-    });
+  inputs.forEach((input) => {
+    input.addEventListener("midimessage", handleInput);
+  });
 }
 
 function handleInput(input) {
-    const command = input.data[0];
-    const channel = command & 0x0F;
+  const command = input.data[0];
+  const channel = command & 0x0f;
 
-    if (channel === 4) {
-      const note = input.data[1];
-      const velocity = input.data[2];
-    
+  if (channel === 4) {
+    const note = input.data[1];
+    const velocity = input.data[2];
+
     switch (command) {
-        case 144:
+      case 144:
         if (velocity > 0) {
-            noteOn(note, velocity);
+          noteOn(note, velocity);
         } else {
-            noteOff(note);
+          noteOff(note);
         }
         break;
-        case 128:
-            noteOff(note);
-            break;
-        }
+      case 128:
+        noteOff(note);
+        break;
     }
+  }
 }
 
 function noteOn(note, velocity) {
-    const osc = ctx.createOscillator();
+  const osc = ctx.createOscillator();
 
-    const oscGain = ctx.createGain();
-    oscGain.gain.value = 0.33;
+  const oscGain = ctx.createGain();
+  oscGain.gain.value = 0.33;
 
-    const velocityGainAmount = (1 / 127) * velocity;
-    const velocityGain = ctx.createGain();
-    velocityGain.gain.value = velocityGainAmount;
+  const velocityGainAmount = (1 / 127) * velocity;
+  const velocityGain = ctx.createGain();
+  velocityGain.gain.value = velocityGainAmount;
 
-    osc.type = "sawtooth";
-    osc.frequency.value = midiToFreq(note);
+  osc.type = "sawtooth";
+  osc.frequency.value = midiToFreq(note);
 
-    osc.connect(oscGain);
-    oscGain.connect(velocityGain);
-    velocityGain.connect(ctx.destination);
+  osc.connect(oscGain);
+  oscGain.connect(velocityGain);
+  velocityGain.connect(ctx.destination);
 
-    osc.gain = oscGain;
+  osc.gain = oscGain;
 
-    oscillators[note.toString()] = osc;
-    console.log(oscillators);
-    osc.start();
+  oscillators[note.toString()] = osc;
+  console.log(oscillators);
+  osc.start();
 }
 
 function noteOff(note) {
-    const osc = oscillators[note.toString()];
-    const oscGain = osc.gain;
+  const osc = oscillators[note.toString()];
+  const oscGain = osc.gain;
 
-    oscGain.gain.setValueAtTime(oscGain.gain.value, ctx.currentTime);
-    oscGain.gain.exponentialRampToValueAtTime(0.0001, ctx.currentTime + 0.0001);
+  oscGain.gain.setValueAtTime(oscGain.gain.value, ctx.currentTime);
+  oscGain.gain.exponentialRampToValueAtTime(0.0001, ctx.currentTime + 0.0001);
 
-    setTimeout(() => {
-        osc.stop();
-        osc.disconnect();
-    }, 2000);
-    delete oscillators[note.toString()];
-    console.log(oscillators);
+  setTimeout(() => {
+    osc.stop();
+    osc.disconnect();
+  }, 2000);
+  delete oscillators[note.toString()];
+  console.log(oscillators);
 }
 
 function fail() {
-    console.log("Could not connect MIDI");
+  console.log("Could not connect MIDI");
 }
 
 function updateDevices(event) {
-    console.log(event);
-    console.log(`Name: ${event.port.name}, Brand: ${event.port.manufacturer}, State: ${event.port.state}, Type: ${event.port.type}`);
+  console.log(event);
+  console.log(
+    `Name: ${event.port.name}, Brand: ${event.port.manufacturer}, State: ${event.port.state}, Type: ${event.port.type}`,
+  );
 }
