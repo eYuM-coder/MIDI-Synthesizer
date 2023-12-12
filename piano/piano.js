9a7d862f3ed5ac7b88fe08681206ff8b567aedd6
Replit user <>
2023-12-12 19:47:18 +0000

    a

--- piano/piano.js
+++
@@ -1,9 +0,0 @@
-import { Keyboard } from './node_modules/piano-keyboard';
-const pianoContainer = document.getElementById('piano');
-const myPiano = new Keyboard(pianoContainer);
-
-function main() {
-  pianoContainer.innerHTML(myPiano);
-}
-
-main();
